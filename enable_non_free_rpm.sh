5e770bca35c2e6f3313d58cd3243754e3019ad95
Peter Stone <peter.stone@blue-nook.com>
2022-10-08 23:55:40 +1030

    Add setup files for Fedora

---
+++ enable_non_free_rpm.sh
@@ -0,0 +1,9 @@
+#!/bin/env bash
+##### CHECK FOR SUDO or ROOT ##################################
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must be run as sudo or root, try again..."
+  exit 1
+fi
+
+dnf install \
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
---
+++ setup_desktop.sh
@@ -0,0 +1,89 @@
+#!/bin/env bash
+##### CHECK FOR SUDO or ROOT ##################################
+if ! [ $(id -u) = 0 ]; then
+  echo "This script must be run as sudo or root, try again..."
+  exit 1
+fi
+
+# Install KDE Packages
+dnf install \
+  @"base-x" \
+  @"Common NetworkManager Submodules" \
+  @"Fonts" \
+  @"Hardware Support" \
+  @"Input Methods" \
+  @Multimedia \
+  @"Printing Support" \
+  ark \
+  bluedevil \
+  breeze-gtk \
+  breeze-icon-theme \
+  cagibi \
+  colord-kde \
+  cups-pk-helper \
+  dolphin \
+  firefox \
+  git \
+  glibc-all-langpacks \
+  gnome-keyring-pam \
+  gwenview \
+  kcalc \
+  kcm_systemd \
+  kde-gtk-config \
+  kde-partitionmanager \
+  kde-print-manager \
+  kde-settings-pulseaudio \
+  kde-style-breeze \
+  kdegraphics-thumbnailers \
+  kdeplasma-addons \
+  kdialog \
+  kdnssd \
+  kf5-akonadi-server \
+  kf5-akonadi-server-mysql \
+  kf5-baloo-file \
+  kf5-kipi-plugins \
+  kfind \
+  khotkeys \
+  kmenuedit \
+  konsole5 \
+  kscreen \
+  kscreenlocker \
+  ksshaskpass \
+  ksysguard \
+  kwalletmanager5 \
+  kwebkitpart \
+  kwin \
+  NetworkManager-config-connectivity-fedora \
+  numix-icon-theme-circle \
+  pam-kwallet \
+  phonon-qt5-backend-gstreamer \
+  pinentry-qt \
+  plasma-breeze \
+  plasma-desktop \
+  plasma-desktop-doc \
+  plasma-drkonqi \
+  plasma-nm \
+  plasma-nm-l2tp \
+  plasma-nm-openconnect \
+  plasma-nm-openswan \
+  plasma-nm-openvpn \
+  plasma-nm-pptp \
+  plasma-nm-vpnc \
+  plasma-pa \
+  plasma-user-manager \
+  plasma-workspace \
+  plasma-workspace-geolocation \
+  polkit-kde \
+  qt5-qtbase-gui \
+  qt5-qtdeclarative \
+  sddm \
+  sddm-breeze \
+  sddm-kcm \
+  setroubleshoot \
+  sni-qt \
+  spectacle \
+  vim \
+  xorg-x11-drv-libinput
+
+systemctl enable sddm
+systemctl set-default graphical.target
