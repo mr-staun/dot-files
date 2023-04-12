97139d0f74748312759aef17047db5300296a238
Peter Stone <peter.stone@blue-nook.com>
2023-04-12 16:41:52 +0930

    Add RPM Fusion free repo

--- Fedora/enable_rpm_fusion.sh
+++ Fedora/enable_rpm_fusion.sh
@@ -8,4 +8,5 @@ fi
 echo "Enabling RPM Fusion Non-Free repository"
 
 dnf install \
-  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+  https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm \
+  https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
