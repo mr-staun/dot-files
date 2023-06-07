d630fc81f06b3cf2fa44489e55819299d4b97d62
Peter Stone <peter.stone@blue-nook.com>
2023-06-07 20:42:19 +0930

    Add terminal colorscheme and profile

--- Fedora/workstation/set_look_and_feel.sh
+++ Fedora/workstation/set_look_and_feel.sh
@@ -1,4 +1,6 @@
-#!/bin/env bash
+#!/bin/bash
+konsole_path=./local/share/konsole
+
 ##### CHECK FOR SUDO or ROOT ##################################
 if ! [ $(id -u) = 0 ]; then
   echo "This script must be run as sudo or root, try again..."
@@ -7,3 +9,8 @@ fi
 
 dnf install \
 numix-icon-theme-circle
+cascadia-code-font
+
+cp ./terminal/inputrc ~/.inputrc
+cp ./terminal/Edna.colorscheme $konsole_path
+cp ./terminal/Blue.profile $konsole_path
---
+++ Fedora/workstation/terminal/Blue.profile
@@ -0,0 +1,8 @@
+[Appearance]
+ColorScheme=Onward
+Font=Cascadia Code,10,-1,5,50,0,0,0,0,0
+
+[General]
+Environment=TERM=xterm-256color,COLORTERM=truecolor
+Name=Blue
+Parent=FALLBACK/
