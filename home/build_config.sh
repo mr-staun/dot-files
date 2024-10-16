09e65860c1a1a3c1f6541e9a5e0a42d9c708ee24
Peter Stone <peter.stone@blue-nook.com>
2024-10-17 09:12:35 +1030

    Comment script to build config

--- home/build_config.sh
+++ home/build_config.sh
@@ -1,6 +1,8 @@
 #/bin/bash
 set -e
 
+
+# Function that copies a file into "~/.[filename]"
 copy_user_home_config_file () {
     if [[ ! -f $1 ]]; then
         echo "File '$1' does not exist or is invalid! Copy failed."
@@ -13,6 +15,7 @@ copy_user_home_config_file () {
 home_config_dir=~/.config
 master_config_dir=./config
 
+# Check flags
 if [[ "$1" == "laptop" ]]; then
     echo "Creating configuration for $1"
 elif [[ "$1" == "workstation" ]]; then
@@ -22,6 +25,7 @@ else
     exit 1
 fi
 
+# Copy basic configuration to home
 copy_user_home_config_file bashrc;
 copy_user_home_config_file inputrc;
 copy_user_home_config_file bash_profile;
@@ -36,11 +40,16 @@ cp -r ${master_config_dir}/* ${home_config_dir}
 echo "Building waybar configuration..."
 
 waybar_config_dir=${home_config_dir}/waybar
+
+# Finds the line number of a specific comment
 comment_line_num=$(grep "// INSERT INITIAL CONFIG HERE" ${waybar_config_dir}/config_base.jsonc -n | cut -f1 -d:)
+# Add initial config for waybar
 head -n $((comment_line_num - 1)) ${waybar_config_dir}/config_base.jsonc > ${waybar_config_dir}/config.jsonc
+# Copy specific config files based on flags
 cat ${waybar_config_dir}/config_$1.jsonc >> ${waybar_config_dir}/config.jsonc
 tail -n +$((comment_line_num + 1)) ${waybar_config_dir}/config_base.jsonc >> ${waybar_config_dir}/config.jsonc
 
+# Delete temporary files
 rm ${waybar_config_dir}/config_base.jsonc
 rm ${waybar_config_dir}/config_*.jsonc
 
