df5fa5067630c400445abb9ded2c1024b6741b8d
Peter Stone <peter.stone@blue-nook.com>
2025-04-24 22:41:48 +0930

    Add partial function to copy config from A to B

--- scripts/build-config.py
+++ scripts/build-config.py
@@ -1,5 +1,6 @@
 import argparse
 import logging
+from pathlib import Path
 import sys
 
 
@@ -17,7 +18,11 @@ def create_logger(log_level: str):
     # Set up handler to output to stdout
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(numeric_level)
-    formatter = logging.Formatter('%(asctime)s [%(filename)s:%(funcName)s] - %(levelname)s: %(message)s')
+    # Only have more logging data when debug is set
+    if numeric_level == 10:
+        formatter = logging.Formatter('%(asctime)s [%(filename)s:%(funcName)s] - %(levelname)s: %(message)s')
+    else:
+        formatter = logging.Formatter('%(message)s')
     handler.setFormatter(formatter)
     logger.addHandler(handler)
 
@@ -44,6 +49,38 @@ def parse_arguments(args):
 def main(args):
     parsed_args = parse_arguments(args)
     logger = create_logger(parsed_args.log_level)
+
+    # Get the scipt and root directory
+    script_dir = Path(__file__).resolve().parent
+    logger.debug(f"Script directory is {script_dir}")
+    root_dir = script_dir.parent()
+    logger.debug(f"Root directory of dot-files is {root_dir}")
+
+    # Get necessary dirs to copy and build config
+    config_dir = root_dir + "config"
+    logger.debug(f"Config directory is {config_dir}")
+    overlays_dir = config_dir + "overlays"
+    logger.debug(f"Overlays directory is {overlays_dir}")
+    home_dir = root_dir + "home"
+    logger.debug(f"Home config directory is {home_dir}")
+
+def copy_dot_files(target_dir: Path, destination_dir: Path, dry_run: bool, logger):
+    if not target_dir.is_dir():
+        raise ValueError(f"Target directory '{destination_dir} is not valid!")
+    
+    if not target_dir.exists():
+        raise FileNotFoundError(f"Target directory '{destination_dir} does not exist!")
+        
+    if not destination_dir.is_dir():
+        raise ValueError(f"Destination directory '{destination_dir} is not valid!")
+    
+    if not destination_dir.exists():
+        raise FileNotFoundError(f"Destination directory '{destination_dir} does not exist!")
+
+    for dot_file in target_dir.rglob("*"):
+        if dot_file.is_dir():
+
+
 
 if __name__ == "__main__":
     main(sys.argv[1:])
